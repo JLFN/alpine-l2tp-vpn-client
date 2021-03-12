f6dd4634672aa388719c5e88dbe86252b8ec0533
Wolffsohn <41384754+Wolffsohn@users.noreply.github.com>
2021-03-12 15:43:21 +0000

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -60,5 +60,7 @@ iptables -t nat -A POSTROUTING -o ppp0 -j MASQUERADE
 iptables -A FORWARD -i ppp0 -o eth0 -m state --state RELATED,ESTABLISHED -j ACCEPT
 iptables -A FORWARD -i eth0 -o eth0 -j ACCEPT
 
+# Wolffsohn - start miniupnpd - uses config in /etc/miniupnpd.conf
+miniupnpd
 
 exec tail -f /dev/null  ## %%LAST-CMD_2_REPLACE%
