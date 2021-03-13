81177252aa26f615a80e264502ec931dfc5816be
Wolffsohn <41384754+Wolffsohn@users.noreply.github.com>
2021-03-13 17:41:18 +0000

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -58,7 +58,7 @@ fi
 echo 1 > /proc/sys/net/ipv4/ip_forward    # or /etc/sysctl.conf  and add net.ipv4.ip_forward = 1
 iptables -t nat -A POSTROUTING -o ppp0 -j MASQUERADE
 iptables -A FORWARD -i ppp0 -o eth0 -m state --state RELATED,ESTABLISHED -j ACCEPT
-iptables -A FORWARD -i eth0 -o eth0 -j ACCEPT
+iptables -A FORWARD -i eth0 -o ppp0 -j ACCEPT
 
 # Wolffsohn - start miniupnpd - uses config in /etc/miniupnpd.conf
 miniupnpd
